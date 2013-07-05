60abe0872b607be148428b7bc226a480544b2f92
TaqiA <taqi_islovely@hotmail.com>
2013-07-05 02:10:23 -0700

    Update dj.js

--- dj.js
+++ dj.js
@@ -5,6 +5,16 @@ $ (function(){
         $(this).hide(2000)
     $("td").click(function(){
        $(this).hide(2000)
+
+       $("div").click(function(){
+
+          $("#a1").hide(3000)
+           $("#a1").css("background-color","red");
+
+
+       });
+
+
     });
 
 
